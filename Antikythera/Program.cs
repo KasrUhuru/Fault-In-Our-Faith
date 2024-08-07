93180ba8bf83d46e433b25af2a4a66deab3516e3
mrivera <matias.g.rivera.civ@gmail.com>
2024-08-07 15:21:29 -0700

    Commented out the clayPits line 366 to prevent build errors.

--- Antikythera/Program.cs
+++ Antikythera/Program.cs
@@ -363,7 +363,8 @@ class Program
         // Instantiate the current room: The Clay Pits
         // The player can LOOK and EXAMINE things
 
-        player.CurrentRoom = clayPits;
+        // FIXME: Find a way to define Character location
+        // player.CurrentRoom = clayPits;
 
         // Create the actual game environment
         Console.WriteLine("It takes you a few moments to finish purging the mud from your belly.");
